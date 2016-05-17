d43f51e3ec9846ff83e05ff4842bd226d1893134
Terng <gopsdepth@hotmail.com>
2016-05-17 18:14:14 +0700

    PLUTO-49 create layout and some behavior complete.

---
+++ src/main/java/com/whitefly/plutocrat/mainmenu/adapters/listeners/OnLoadmoreListener.java
@@ -0,0 +1,8 @@
+package com.whitefly.plutocrat.mainmenu.adapters.listeners;
+
+/**
+ * Created by Satjapot on 5/18/16 AD.
+ */
+public abstract class OnLoadmoreListener {
+    public abstract void onLoad();
+}
