fb10710071dd9e94a10553847a0414757ae59732
Terng <gopsdepth@hotmail.com>
2016-05-18 18:55:13 +0700

    PLUTO-53 fix following PR

--- src/main/java/com/whitefly/plutocrat/mainmenu/fragments/BuyoutFragment.java
+++ src/main/java/com/whitefly/plutocrat/mainmenu/fragments/BuyoutFragment.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,13 +15,32 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.whitefly.plutocrat.R;
+import com.whitefly.plutocrat.helpers.EventBus;
+import com.whitefly.plutocrat.mainmenu.adapters.BuyoutAdapter;
+import com.whitefly.plutocrat.mainmenu.adapters.listeners.OnLoadmoreListener;
+import com.whitefly.plutocrat.mainmenu.events.LoadBuyoutsEvent;
+import com.whitefly.plutocrat.mainmenu.events.LoadTargetsEvent;
+import com.whitefly.plutocrat.mainmenu.views.IBuyoutView;
 import com.whitefly.plutocrat.mainmenu.views.ITabView;
+import com.whitefly.plutocrat.models.BuyoutModel;
+
+import java.util.ArrayList;
 
 /**
  * Created by Satjapot on 5/10/16 AD.
  */
-public class BuyoutFragment extends Fragment implements ITabView {
+public class BuyoutFragment extends Fragment implements ITabView, IBuyoutView {
     public static final String TITLE = "Buyouts";
+    public static final int PER_PAGE = 4;
+
+    // Attributes
+    private BuyoutAdapter mAdapter;
+    private int cpage;
+
+    // Views
+    private RecyclerView mRvMain;
+    private SwipeRefreshLayout mSRLMain;
+
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
@@ -35,6 +57,39 @@ public class BuyoutFragment extends Fragment implements ITabView {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_buyout, container, false);
 
+        // Get Views
+        mRvMain = (RecyclerView) root.findViewById(R.id.rv_players);
+        mSRLMain = (SwipeRefreshLayout) root.findViewById(R.id.srl_players);
+        // Initiate
+        ArrayList<BuyoutModel> dataset = new ArrayList<>();
+        dataset.add(null);
+
+        mRvMain.setHasFixedSize(true);
+        mRvMain.setLayoutManager(new LinearLayoutManager(getContext()));
+        mRvMain.setNestedScrollingEnabled(true);
+        mAdapter = new BuyoutAdapter(getActivity(), dataset);
+        mRvMain.setAdapter(mAdapter);
+
+        // Get Adapter
+        cpage = 1;
+        EventBus.getInstance().post(new LoadBuyoutsEvent(cpage, PER_PAGE));
+
+        // Event Handler
+        mSRLMain.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                // Loading new list
+                cpage = 1;
+                EventBus.getInstance().post(new LoadBuyoutsEvent(cpage, PER_PAGE));
+            }
+        });
+        mAdapter.setOnLoadmoreListener(new OnLoadmoreListener() {
+            @Override
+            public void onLoad() {
+                EventBus.getInstance().post(new LoadBuyoutsEvent(++cpage, PER_PAGE));
+            }
+        });
+
         return root;
     }
 
@@ -46,5 +101,16 @@ public class BuyoutFragment extends Fragment implements ITabView {
     @Override
     public String getTitle() {
         return TITLE;
+    }
+
+    @Override
+    public void setTargetList(ArrayList<BuyoutModel> items) {
+        if(cpage == 1) {
+            mAdapter.getDataSet().clear();
+        }
+        mAdapter.addItems(items);
+
+        // Switch off all loading widget
+        mSRLMain.setRefreshing(false);
     }
 }
