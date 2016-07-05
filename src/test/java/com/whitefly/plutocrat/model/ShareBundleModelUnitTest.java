f680d353b04104f2fd61d71d5fa219a9f7e22bbd
Satjapot <gopsdepth@hotmail.com>
2016-07-05 13:46:15 +0700

    PLUTO-99 Change the shares price in Shares page

--- src/test/java/com/whitefly/plutocrat/model/ShareBundleModelUnitTest.java
+++ src/test/java/com/whitefly/plutocrat/model/ShareBundleModelUnitTest.java
@@ -3,6 +3,7 @@ package com.whitefly.plutocrat.model;
 import com.whitefly.plutocrat.models.IAPItemDetailModel;
 import com.whitefly.plutocrat.models.ShareBundleModel;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -13,83 +14,114 @@ import static org.junit.Assert.*;
  */
 @RunWith(JUnit4.class)
 public class ShareBundleModelUnitTest {
+    private static final double MILLION = Math.pow(10., 6.);
+
+    private String[] mExpectedProductIds;
+    private String[] mExpectedPrices;
+    private String[] mExpectedTotals;
+    private String mCurrencySymbol;
+    private int[] mQuantities;
+
+    @Before
+    public void setupExpectedValue() {
+        mExpectedProductIds = new String[] {
+                "com.whiteflyventuresinc.plutocrat.shares.1",
+                "com.whiteflyventuresinc.plutocrat.shares.10",
+                "com.whiteflyventuresinc.plutocrat.shares.50",
+                "com.whiteflyventuresinc.plutocrat.shares.100",
+                "com.whiteflyventuresinc.plutocrat.shares.500",
+        };
+        mExpectedPrices = new String[] {
+                "1.00", "1.00", "0.90", "0.80", "0.60"
+        };
+        mExpectedTotals = new String[] {
+                "0.99", "9.99", "44.99", "79.99", "299.00"
+        };
+        mQuantities = new int[] { 1, 10, 50, 100, 500 };
+    }
 
     @Test
     public void testGetPriceStringWithRawConstructor() {
-        String expectedProductId = "test.product.id";
-        String expectedTotal = "$49.50";
-        String expectedPrice = "$0.99";
+        mCurrencySymbol = "$";
 
-        int qty = 50;
-        float price = 0.99f;
+        for(int i=0, n=mExpectedProductIds.length; i<n; i++) {
+            String expectedProductId = mExpectedProductIds[i];
+            String expectedPrice = mCurrencySymbol + mExpectedPrices[i];
+            String expectedTotal = mCurrencySymbol + mExpectedTotals[i];
 
-        ShareBundleModel model = new ShareBundleModel(expectedProductId, qty, price);
+            int qty = mQuantities[i];
+            float price = Float.valueOf(mExpectedTotals[i]);
 
-        assertEquals(expectedProductId, model.sku);
-        assertEquals(expectedPrice, model.getPrice());
-        assertEquals(expectedTotal, model.getTotalPrice());
+            ShareBundleModel model = new ShareBundleModel(expectedProductId, qty, price);
+
+            assertEquals(expectedProductId, model.sku);
+            assertEquals(expectedPrice, model.getPrice());
+            assertEquals(expectedTotal, model.getTotalPrice());
+        }
     }
 
     @Test
     public void testGetPriceStringWithIAPItemDetailModelConstructor() {
-        String expectedProductId = "test.product.id";
-        String expectedTotal = "$49.50";
-        String expectedPrice = "$0.99";
+        mCurrencySymbol = "$";
+        String currency = "USD";
 
-        IAPItemDetailModel inputModel = new IAPItemDetailModel();
-        inputModel.productId = expectedProductId;
-        inputModel.title = "some title";
-        inputModel.description = "50";
-        inputModel.type = "inapp";
-        inputModel.price = "$49.50";
-        inputModel.priceAmountMicro = "49500000";
-        inputModel.priceCurrencyCode = "USD";
+        for(int i=0, n=mExpectedProductIds.length; i<n; i++) {
+            String expectedProductId = mExpectedProductIds[i];
+            String expectedPrice = mCurrencySymbol + mExpectedPrices[i];
+            String expectedTotal = mCurrencySymbol + mExpectedTotals[i];
+            int quantity = mQuantities[i];
 
-        ShareBundleModel model = new ShareBundleModel(inputModel);
+            IAPItemDetailModel inputModel = new IAPItemDetailModel();
+            inputModel.productId = expectedProductId;
+            inputModel.title = "some title";
+            inputModel.description = String.valueOf(quantity);
+            inputModel.type = "inapp";
+            inputModel.price = expectedPrice;
+            inputModel.priceAmountMicro = String.valueOf((long) (Double.parseDouble(mExpectedTotals[i]) * MILLION));
+            inputModel.priceCurrencyCode = currency;
 
-        assertEquals(expectedPrice, model.getPrice());
-        assertEquals(expectedTotal, model.getTotalPrice());
+            ShareBundleModel model = new ShareBundleModel(inputModel);
+
+            assertEquals(expectedProductId, model.sku);
+            assertEquals(expectedPrice, model.getPrice());
+            assertEquals(expectedTotal, model.getTotalPrice());
+        }
     }
 
     @Test
     public void testGetPriceStringWithLocalizedIAPItemDetailModelConstructor() {
-        String expectedProductId = "test.product.id";
-        String expectedTotal = "THB5";
-        String expectedPrice = "THB2.50";
+        mCurrencySymbol = "THB";
+        String currency = "THB";
+        mExpectedPrices = new String[] {
+                "35.00", "32.50", "31.50", "28.00", "21.00"
+        };
+        mExpectedTotals = new String[] {
+                "35.00", "325.00", "1,575.00", "2,800.00", "10,500.00"
+        };
+        String[] totalsWithoutComma = new String [] {
+                "35.00", "325.00", "1575.00", "2800.00", "10500.00"
+        };
 
-        IAPItemDetailModel inputModel = new IAPItemDetailModel();
-        inputModel.productId = expectedProductId;
-        inputModel.title = "some title";
-        inputModel.description = "2";
-        inputModel.type = "inapp";
-        inputModel.price = "THB5.00";
-        inputModel.priceAmountMicro = "5000000";
-        inputModel.priceCurrencyCode = "THB";
+        for(int i=0, n=mExpectedProductIds.length; i<n; i++) {
+            String expectedProductId = mExpectedProductIds[i];
+            String expectedPrice = mCurrencySymbol + mExpectedPrices[i];
+            String expectedTotal = mCurrencySymbol + mExpectedTotals[i];
+            int quantity = mQuantities[i];
 
-        ShareBundleModel model = new ShareBundleModel(inputModel);
+            IAPItemDetailModel inputModel = new IAPItemDetailModel();
+            inputModel.productId = expectedProductId;
+            inputModel.title = "some title";
+            inputModel.description = String.valueOf(quantity);
+            inputModel.type = "inapp";
+            inputModel.price = expectedPrice;
+            inputModel.priceAmountMicro = String.valueOf((long) (Double.parseDouble(totalsWithoutComma[i]) * MILLION));
+            inputModel.priceCurrencyCode = currency;
 
-        assertEquals(expectedPrice, model.getPrice());
-        assertEquals(expectedTotal, model.getTotalPrice());
-    }
+            ShareBundleModel model = new ShareBundleModel(inputModel);
 
-    @Test
-    public void testGetPriceStringWithLocalizedANDCommaIAPItemDetailModelConstructor() {
-        String expectedProductId = "test.product.id";
-        String expectedTotal = "THB1,000";
-        String expectedPrice = "THB500";
-
-        IAPItemDetailModel inputModel = new IAPItemDetailModel();
-        inputModel.productId = expectedProductId;
-        inputModel.title = "some title";
-        inputModel.description = "2";
-        inputModel.type = "inapp";
-        inputModel.price = "THB1,000.00";
-        inputModel.priceAmountMicro = "1000000000";
-        inputModel.priceCurrencyCode = "THB";
-
-        ShareBundleModel model = new ShareBundleModel(inputModel);
-
-        assertEquals(expectedPrice, model.getPrice());
-        assertEquals(expectedTotal, model.getTotalPrice());
+            assertEquals(expectedProductId, model.sku);
+            assertEquals(expectedPrice, model.getPrice());
+            assertEquals(expectedTotal, model.getTotalPrice());
+        }
     }
 }
